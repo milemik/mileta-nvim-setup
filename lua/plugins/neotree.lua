4687348bde4016fc08f4bce48b5f2a32e01cb55d
milemik <milemik68@gmail.com>
2025-04-02 21:14:38 +0200

    Added neotree custom command

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -5,14 +5,14 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
-    -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   lazy = false, -- neo-tree will lazily load itself
   ---@module "neo-tree"
   ---@type neotree.Config?
   opts = {
-    -- fill any relevant options here
   },
-} 
-  
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
+  end
+}
 
